22f36fe7de4d65564c945df0d9caaaccbf51e8f3
YangXin0205 <38850750+YangXin0205@users.noreply.github.com>
2018-05-05 14:58:29 +0800

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,44 +1,38 @@
 from skimage import io,transform
 import tensorflow as tf
 import numpy as np
+import os
+import glob
+import shutil
 
+flower_dict = {}
+trian_pic = "E:\\data\\dataset\\man_woman\\train\\"
+for idx,dir in enumerate(os.listdir(trian_pic)):
+    flower_dict[idx] = dir
 
-path1 = r"E:\data\dataset\flower_photos\yujinxiang\1.jpg"
-path2 = r"E:\data\dataset\flower_photos\yujinxiang\2.jpg"
-path3 = r"E:\data\dataset\flower_photos\yujinxiang\3.jpg"
-path4 = r"E:\data\dataset\flower_photos\yujinxiang\4.jpg"
-path5 = r"E:\data\dataset\flower_photos\yujinxiang\5.jpg"
-
-flower_dict = {0:'dasiy',1:'dandelion',2:'roses',3:'sunflowers',4:'tulips'}
-
-w=100
-h=100
-c=3
 
 def read_one_image(path):
     img = io.imread(path)
-    img = transform.resize(img,(w,h))
+    img = transform.resize(img,(100,100))
     return np.asarray(img)
 
 with tf.Session() as sess:
     data = []
-    data1 = read_one_image(path1)
-    data2 = read_one_image(path2)
-    data3 = read_one_image(path3)
-    data4 = read_one_image(path4)
-    data5 = read_one_image(path5)
-    data.append(data1)
-    data.append(data2)
-    data.append(data3)
-    data.append(data4)
-    data.append(data5)
+    pic_name = []
+    test_pic = "E:\\data\\dataset\\man_woman\\test\\"
+    for i in os.listdir(test_pic):
+        if os.path.isfile(os.path.join(test_pic,i)):
+            pic_name.append(i)
+            data.append(read_one_image(os.path.join(test_pic,i)))
 
-    saver = tf.train.import_meta_graph('E:\\tensorflow\\cnn_classifier\\logs\\model.ckpt.meta')
-    saver.restore(sess,tf.train.latest_checkpoint('E:\\tensorflow\\cnn_classifier\\logs\\'))
+    saver = tf.train.import_meta_graph(glob.glob('E:\\tensorflow\\AlexNet\\logs\\model.ckpt-*.meta')[0])
+    saver.restore(sess,tf.train.latest_checkpoint('E:\\tensorflow\\AlexNet\\logs\\'))
 
     graph = tf.get_default_graph()
     x = graph.get_tensor_by_name("x:0")
-    feed_dict = {x:data}
+    kp= graph.get_tensor_by_name("kp:0")
+
+    feed_dict = {x:data,kp:1.0}
 
     logits = graph.get_tensor_by_name("logits_eval:0")
 
@@ -51,5 +45,16 @@ with tf.Session() as sess:
     #根据索引通过字典对应花的分类
     output = []
     output = tf.argmax(classification_result,1).eval()
+
+
     for i in range(len(output)):
         print("第",i+1,"朵花预测:"+flower_dict[output[i]])
+        classes = flower_dict[output[i]]
+        #测试图片abs路径
+        path1 = os.path.join(test_pic,classes)
+
+        if not os.path.exists(path1):
+            os.mkdir(path1)
+            os.renames(os.path.join(test_pic, pic_name[i]), os.path.join(path1, pic_name[i]))
+        else:
+            os.renames(os.path.join(test_pic, pic_name[i]), os.path.join(path1, pic_name[i]))
