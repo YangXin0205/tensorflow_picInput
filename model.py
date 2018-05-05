2a3fc3f7a39428869a098b63459944839d17a68d
YangXin0205 <38850750+YangXin0205@users.noreply.github.com>
2018-05-05 14:59:21 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-def inference(input_tensor,regularizer):
+def inference(input_tensor,regularizer,keep_pro):
     with tf.variable_scope('layer1-conv1'):
         conv1_weights = tf.get_variable("weight",[5,5,3,32],initializer=tf.truncated_normal_initializer(stddev=0.1))
         conv1_biases = tf.get_variable("bias", [32], initializer=tf.constant_initializer(0.0))
@@ -25,24 +25,22 @@ def inference(input_tensor,regularizer):
         conv3 = tf.nn.conv2d(pool2, conv3_weights, strides=[1, 1, 1, 1], padding='SAME')
         relu3 = tf.nn.relu(tf.nn.bias_add(conv3, conv3_biases))
 
-    # with tf.name_scope("layer6-pool3"):
-    #     pool3 = tf.nn.max_pool(relu3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-    #
     with tf.variable_scope("layer6-conv4"):
-        conv4_weights = tf.get_variable("weight",[3,3,128,128],initializer=tf.truncated_normal_initializer(stddev=0.1))
-        conv4_biases = tf.get_variable("bias", [128], initializer=tf.constant_initializer(0.0))
+        conv4_weights = tf.get_variable("weight",[3,3,128,256],initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv4_biases = tf.get_variable("bias", [256], initializer=tf.constant_initializer(0.0))
         conv4 = tf.nn.conv2d(relu3, conv4_weights, strides=[1, 1, 1, 1], padding='SAME')
         relu4 = tf.nn.relu(tf.nn.bias_add(conv4, conv4_biases))
 
     with tf.variable_scope("layer7-conv5"):
-        conv5_weights = tf.get_variable("weight",[3,3,128,256],initializer=tf.truncated_normal_initializer(stddev=0.1))
-        conv5_biases = tf.get_variable("bias", [256], initializer=tf.constant_initializer(0.0))
+        conv5_weights = tf.get_variable("weight",[3,3,256,512],initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv5_biases = tf.get_variable("bias", [512], initializer=tf.constant_initializer(0.0))
         conv5 = tf.nn.conv2d(relu4, conv5_weights, strides=[1, 1, 1, 1], padding='SAME')
         relu5 = tf.nn.relu(tf.nn.bias_add(conv5, conv5_biases))
+        drop5 = tf.nn.dropout(relu5, keep_pro)
 
     with tf.name_scope("layer8-pool3"):
-        pool4 = tf.nn.max_pool(relu5, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-        nodes = 12*12*256
+        pool4 = tf.nn.max_pool(drop5, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+        nodes = 12*12*512
         reshaped = tf.reshape(pool4,[-1,nodes])
 
     with tf.variable_scope('layer10-fc1'):
@@ -52,22 +50,22 @@ def inference(input_tensor,regularizer):
         fc1_biases = tf.get_variable("bias", [1024], initializer=tf.constant_initializer(0.1))
 
         fc1 = tf.nn.relu(tf.matmul(reshaped, fc1_weights) + fc1_biases)
-        fc1 = tf.nn.dropout(fc1, 0.5)
+        fc1 = tf.nn.dropout(fc1, keep_pro)
 
     with tf.variable_scope('layer11-fc2'):
-        fc2_weights = tf.get_variable("weight", [1024, 512],
+        fc2_weights = tf.get_variable("weight", [1024, 1024],
                                       initializer=tf.truncated_normal_initializer(stddev=0.1))
         if regularizer != None: tf.add_to_collection('losses', regularizer(fc2_weights))
-        fc2_biases = tf.get_variable("bias", [512], initializer=tf.constant_initializer(0.1))
+        fc2_biases = tf.get_variable("bias", [1024], initializer=tf.constant_initializer(0.1))
 
         fc2 = tf.nn.relu(tf.matmul(fc1, fc2_weights) + fc2_biases)
-        fc2 = tf.nn.dropout(fc2, 0.5)
+        fc2 = tf.nn.dropout(fc2, keep_pro)
 
     with tf.variable_scope('layer12-fc3'):
-        fc3_weights = tf.get_variable("weight", [512, 5],
+        fc3_weights = tf.get_variable("weight", [1024, 2],
                                       initializer=tf.truncated_normal_initializer(stddev=0.1))
         if regularizer != None: tf.add_to_collection('losses', regularizer(fc3_weights))
-        fc3_biases = tf.get_variable("bias", [5], initializer=tf.constant_initializer(0.1))
+        fc3_biases = tf.get_variable("bias", [2], initializer=tf.constant_initializer(0.1))
         logit = tf.matmul(fc2, fc3_weights) + fc3_biases
 
     return logit
