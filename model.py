ab641ed41da4995acfae05fd7949885b1234899f
YangXin0205 <38850750+YangXin0205@users.noreply.github.com>
2018-05-23 15:23:54 +0800

    Update model.py

--- model.py
+++ model.py
@@ -12,65 +12,80 @@ def variable_summaries(var):
         tf.summary.scalar('min', tf.reduce_min(var))  # 最小值
         tf.summary.histogram('histogram', var)  # 直方图
 
-def inference(input_tensor,regularizer,keep_pro):
+def inference(input_tensor,batch_size,regularizer,keep_pro):
     with tf.variable_scope('layer1-conv1'):
         with tf.name_scope('weights'):
-            conv1_weights = tf.get_variable("weight",[5,5,3,32],initializer=tf.truncated_normal_initializer(stddev=0.1))
+            conv1_weights = tf.get_variable("weight",
+                                            [5,5,3,32],
+                                            initializer=tf.truncated_normal_initializer(stddev=0.1))
             variable_summaries(conv1_weights)
         with tf.name_scope('biases'):
-            conv1_biases = tf.get_variable("bias", [32], initializer=tf.constant_initializer(0.0))
+            conv1_biases = tf.get_variable("bias",
+                                           [32],
+                                           initializer=tf.constant_initializer(0.1))
             variable_summaries(conv1_biases)
         conv1 = tf.nn.conv2d(input_tensor, conv1_weights, strides=[1, 1, 1, 1], padding='SAME')
         relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))
 
     with tf.name_scope("layer2-pool1"):
-        pool1 = tf.nn.max_pool(relu1, ksize = [1,2,2,1],strides=[1,2,2,1],padding="VALID")
+        pool1 = tf.nn.max_pool(relu1, ksize = [1,2,2,1],strides=[1,2,2,1],padding="VALID",name='pooling1')
 
     with tf.variable_scope("layer3-conv2"):
         conv2_weights = tf.get_variable("weight",[5,5,32,64],initializer=tf.truncated_normal_initializer(stddev=0.1))
         variable_summaries(conv2_weights)
-        conv2_biases = tf.get_variable("bias", [64], initializer=tf.constant_initializer(0.0))
+        conv2_biases = tf.get_variable("bias", [64], initializer=tf.constant_initializer(0.1))
         variable_summaries(conv2_biases)
         conv2 = tf.nn.conv2d(pool1, conv2_weights, strides=[1, 1, 1, 1], padding='SAME')
         relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_biases))
 
     with tf.name_scope("layer4-pool2"):
-        pool2 = tf.nn.max_pool(relu2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+        pool2 = tf.nn.max_pool(relu2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID',name='pooling2')
 
     with tf.variable_scope("layer5-conv3"):
-        conv3_weights = tf.get_variable("weight",[3,3,64,128],initializer=tf.truncated_normal_initializer(stddev=0.1))
-        conv3_biases = tf.get_variable("bias", [128], initializer=tf.constant_initializer(0.0))
+        conv3_weights = tf.get_variable("weight",
+                                        [3,3,64,128],
+                                        initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv3_biases = tf.get_variable("bias", [128], initializer=tf.constant_initializer(0.1))
         conv3 = tf.nn.conv2d(pool2, conv3_weights, strides=[1, 1, 1, 1], padding='SAME')
         relu3 = tf.nn.relu(tf.nn.bias_add(conv3, conv3_biases))
 
     with tf.variable_scope("layer6-conv4"):
-        conv4_weights = tf.get_variable("weight",[3,3,128,256],initializer=tf.truncated_normal_initializer(stddev=0.1))
-        conv4_biases = tf.get_variable("bias", [256], initializer=tf.constant_initializer(0.0))
+        conv4_weights = tf.get_variable("weight",
+                                        [3,3,128,256],
+                                        initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv4_biases = tf.get_variable("bias", [256], initializer=tf.constant_initializer(0.1))
         conv4 = tf.nn.conv2d(relu3, conv4_weights, strides=[1, 1, 1, 1], padding='SAME')
         relu4 = tf.nn.relu(tf.nn.bias_add(conv4, conv4_biases))
 
     with tf.variable_scope("layer7-conv5"):
-        conv5_weights = tf.get_variable("weight",[3,3,256,512],initializer=tf.truncated_normal_initializer(stddev=0.1))
-        conv5_biases = tf.get_variable("bias", [512], initializer=tf.constant_initializer(0.0))
+        conv5_weights = tf.get_variable("weight",
+                                        [3,3,256,512],
+                                        initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv5_biases = tf.get_variable("bias", [512], initializer=tf.constant_initializer(0.1))
         conv5 = tf.nn.conv2d(relu4, conv5_weights, strides=[1, 1, 1, 1], padding='SAME')
         relu5 = tf.nn.relu(tf.nn.bias_add(conv5, conv5_biases))
         drop5 = tf.nn.dropout(relu5, keep_pro)
 
     with tf.name_scope("layer8-pool3"):
-        pool4 = tf.nn.max_pool(drop5, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-        nodes = 8*8*512
-        reshaped = tf.reshape(pool4,[-1,nodes])
+        pool3 = tf.nn.max_pool(drop5, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID',name='pooling3')
 
-    with tf.variable_scope('layer10-fc1'):
-        fc1_weights = tf.get_variable("weight", [nodes, 1024],
-                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
+
+    with tf.variable_scope('layer9-fc1'):
+        reshape = tf.reshape(pool3, shape=[batch_size, -1])
+        #dim = reshape.get_shape()[1].value
+        nodes = 8 * 8 * 512
+        fc1_weights = tf.get_variable('weights',
+                                  shape=[nodes,1024],
+                                  dtype=tf.float32,
+                                  initializer=tf.truncated_normal_initializer(stddev=0.005,dtype=tf.float32))
+
         if regularizer != None: tf.add_to_collection('losses', regularizer(fc1_weights))
         fc1_biases = tf.get_variable("bias", [1024], initializer=tf.constant_initializer(0.1))
 
-        fc1 = tf.nn.relu(tf.matmul(reshaped, fc1_weights) + fc1_biases)
+        fc1 = tf.nn.relu(tf.matmul(reshape, fc1_weights) + fc1_biases)
         fc1 = tf.nn.dropout(fc1, keep_pro)
 
-    with tf.variable_scope('layer11-fc2'):
+    with tf.variable_scope('layer10-fc2'):
         fc2_weights = tf.get_variable("weight", [1024, 1024],
                                       initializer=tf.truncated_normal_initializer(stddev=0.1))
         if regularizer != None: tf.add_to_collection('losses', regularizer(fc2_weights))
@@ -79,7 +94,7 @@ def inference(input_tensor,regularizer,keep_pro):
         fc2 = tf.nn.relu(tf.matmul(fc1, fc2_weights) + fc2_biases)
         fc2 = tf.nn.dropout(fc2, keep_pro)
 
-    with tf.variable_scope('layer12-fc3'):
+    with tf.variable_scope('layer11-softmax'):
         fc3_weights = tf.get_variable("weight", [1024, 2],
                                       initializer=tf.truncated_normal_initializer(stddev=0.1))
         if regularizer != None: tf.add_to_collection('losses', regularizer(fc3_weights))
